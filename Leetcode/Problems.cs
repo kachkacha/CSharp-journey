536fad260ad32df29e32cad2b6936ab3cdf3c328
kachkacha <beqabeqa54321@gmail.com>
2022-12-13 02:18:04 +0400

    polish

--- Leetcode/Problems.cs
+++ Leetcode/Problems.cs
@@ -8,7 +8,20 @@ namespace Leetcode
 {
     internal class Problems
     {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
 
+        //https://leetcode.com/problems/container-with-most-water/
         public int MaxArea(int[] height)
         {
             int max = 0, left = 0, right = height.Length - 1, tmp;
@@ -22,6 +35,7 @@ namespace Leetcode
             return max;
         }
 
+        //https://leetcode.com/problems/valid-parentheses/
         public bool IsValid(string s)
         {
             if (s.Length % 2 == 1)
@@ -55,6 +69,7 @@ namespace Leetcode
             return chars.Count == 0;
         }
 
+        //https://leetcode.com/problems/remove-element/
         public int RemoveElement(int[] nums, int val)
         {
             int count = 0;
@@ -81,6 +96,7 @@ namespace Leetcode
             return nums.Length - count;
         }
 
+        //https://leetcode.com/problems/divide-two-integers/
         public int Divide(int dividend, int divisor)
         {
             long res = (long)dividend / (long)divisor;
@@ -89,6 +105,7 @@ namespace Leetcode
             return (int)res;
         }
 
+        //https://leetcode.com/problems/roman-to-integer/
         public int RomanToInt(string s)
         {
             var dict = new Dictionary<char, int>(){
@@ -108,6 +125,7 @@ namespace Leetcode
             return num;
         }
 
+        //https://leetcode.com/problems/longest-common-prefix/
         public string LongestCommonPrefix(string[] strs)
         {
             StringBuilder res = new("");
@@ -125,6 +143,7 @@ namespace Leetcode
             }
         }
 
+        //https://leetcode.com/problems/zigzag-conversion/
         public String Convert(String s, int n)
         {
             if (n == 1) return s;
@@ -149,6 +168,7 @@ namespace Leetcode
             return result.ToString();
         }
 
+        //https://leetcode.com/problems/remove-duplicates-from-sorted-array/
         public int RemoveDuplicates(int[] nums)
         {
             int count = 1;
@@ -163,6 +183,7 @@ namespace Leetcode
             return count;
         }
 
+        //https://leetcode.com/problems/search-insert-position/
         public int SearchInsert(int[] nums, int target)
         {
             int index = 0;
@@ -177,6 +198,7 @@ namespace Leetcode
             return index;
         }
 
+        //https://leetcode.com/problems/length-of-last-word/
         public int LengthOfLastWord(string s)
         {
             int count = 0;
@@ -197,6 +219,7 @@ namespace Leetcode
             return count;
         }
 
+        //https://leetcode.com/problems/plus-one/
         public int[] PlusOne(int[] digits)
         {
             List<int> result = new List<int>();
@@ -227,6 +250,109 @@ namespace Leetcode
 
             result.Reverse();
             return result.ToArray();
+        }
+
+        //https://leetcode.com/problems/symmetric-tree/
+        public bool IsSymmetric(TreeNode root)
+        {
+            return root is null ? true : IsSymmetricHelper(root.left, root.right);
+        }
+
+        public bool IsSymmetricHelper(TreeNode l, TreeNode r)
+        {
+            if (l is null && r is null) return true; 
+            if ((l is null || r is null) && (l != r)) return false;
+            return (l is null || r is null) && (l == r) || (l.val == r.val && IsSymmetricHelper(l.left, r.right) && IsSymmetricHelper(l.right, r.left));
+        }
+
+        //https://leetcode.com/problems/maximum-depth-of-binary-tree/
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+
+        //https://leetcode.com/problems/same-tree/
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null) return true;
+            if ((p == null && q != null) || (q == null && p != null)) return false;
+            return p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+
+        //https://leetcode.com/problems/climbing-stairs/
+        public int ClimbStairs(int n)
+        {
+            int[] arr = new int[n + 1];
+            arr[0] = 1;
+            arr[1] = 2;
+            for (int i = 2; i <= n; i++)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            return arr[n - 1];
+        }
+
+        //https://leetcode.com/problems/merge-sorted-array/
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int[] arr = new int[m + n];
+            int i = 0, j = 0, k = 0;
+            while (i < m && j < n)
+            {
+                if (nums1[i] < nums2[j])
+                {
+                    arr[k++] = nums1[i++];
+                }
+                else
+                {
+                    arr[k++] = nums2[j++];
+                }
+            }
+            while (i < m)
+            {
+                arr[k++] = nums1[i++];
+            }
+
+            while (j < n)
+            {
+                arr[k++] = nums2[j++];
+            }
+            for (k = 0; k < m + n;)
+            {
+                nums1[k] = arr[k++];
+            }
+        }
+
+        //https://leetcode.com/problems/add-binary/
+        public string AddBinary(string a, string b)
+        {
+            StringBuilder builder = new();
+
+            int rm = 0;
+
+            for (int i = 1; i < Math.Max(a.Length, b.Length) + 1; i++)
+            {
+                int aa = a.Length - i >= 0 ? a[a.Length - i] - '0' : 0;
+                int bb = b.Length - i >= 0 ? b[b.Length - i] - '0' : 0;
+                int sum = aa + bb + rm;
+                if (sum > 1)
+                {
+                    sum = sum % 2;
+                    rm = 1;
+                }
+                else
+                {
+                    rm = 0;
+                }
+                builder.Append(sum);
+            }
+
+            if (rm > 0) builder.Append(rm);
+
+            var res = builder.ToString().ToCharArray();
+            Array.Reverse(res);
+            return new string(res);
         }
 
     }
